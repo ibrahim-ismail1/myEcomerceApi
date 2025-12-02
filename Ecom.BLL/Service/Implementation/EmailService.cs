110dadecef86a136ae907b711398d49e85f548cd
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2025-12-02 23:57:38 +0200

    Removing secrets from Email service

--- Ecom.BLL/Service/Implementation/EmailService.cs
+++ Ecom.BLL/Service/Implementation/EmailService.cs
@@ -6,17 +6,10 @@ namespace Ecom.BLL.Service.Implementation
 {
     public class EmailService : IEmailService
     {
-        //private readonly string _apiKey = "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";
-        //private readonly string _fromEmail = "no-reply@yourdomain.com";
-        //private readonly string _fromName = "Ecom App";
-
         private readonly EmailConfig _emailConfig;
 
         public EmailService(IOptions<EmailConfig> emailConfig)
         {
-            //_apiKey = config["SendGrid:ApiKey"];
-            //_fromEmail = config["SendGrid:FromEmail"];
-            //_fromName = config["SendGrid:FromName"];
             _emailConfig = emailConfig.Value;
         }
 
